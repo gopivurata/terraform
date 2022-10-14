cb418b901309740ef4e89f87ae018f1c39e74976
gopivurata <113236420+gopivurata@users.noreply.github.com>
2022-10-14 18:59:59 +0530

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -2,6 +2,10 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "first_bucket"  {
-    bucket = "firstbuckettf"
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.10.0.0/16"
+    tags = {
+      "Name" = "myvpctf"
+    }
+
 }
