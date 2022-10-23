c1f5e3e06b7273f5820b538df2f82fccfda5f777
gopi vurata <gopivurata1992@gmail.com>
2022-10-23 23:39:40 +0530

    added groovy file

---
+++ vars/terraform.groovy
@@ -0,0 +1,25 @@
+def call(){
+    pipeline {
+    agent  { label 'NODE' }
+    
+    stages {
+        stage('git checkout') {
+            steps {
+               git branch: 'main', url: 'https://github.com/gopivurata/terraform.git'
+            }
+
+        }
+        stage('init') {
+            steps {
+                sh 'terraform init'
+            }
+
+        }
+        stage('apply') {
+            steps {
+               // sh 'terraform apply -auto-approve'
+                sh 'terraform destroy -auto-approve'
+            }
+
+        }
+}
